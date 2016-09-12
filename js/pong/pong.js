24657826eb889b5a11d2caa0e062285f12e9fa4f
Alex <tempus.studios.contact@gmail.com>
2016-09-11 22:01:08 -0700

    Add files via upload

---
+++ js/pong/pong.js
@@ -0,0 +1,16 @@
+function centerCanvas(cnv){
+	
+}
+
+function setup(){
+	var cnv = createCanvas(600, 400);
+	// var x = (windowWidth - width) / 2;
+	// var y = (windowHeight - height) / 2;
+	// cnv.position(x, y);
+	// centerCanvas(cnv);
+	console.log("Hello");
+}
+
+function draw(){
+	background(51);
+}
